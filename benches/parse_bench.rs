178ec1db2171e17a0103d3ff70641b23ff0273cd
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-06-26 18:03:29 +0530

    feat: add criterion parser benchmarks

---
+++ benches/parse_bench.rs
@@ -0,0 +1,27 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use oculus::parser::LogParser;
+use oculus::parser::apache::ApacheParser;
+use oculus::parser::json::JsonParser;
+use oculus::parser::nginx::NginxParser;
+
+fn bench_parse(c: &mut Criterion) {
+    let apache = r#"127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /api HTTP/1.1" 200 1234"#;
+    let nginx = r#"127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /api HTTP/1.1" 200 1234"#;
+    let json = r#"{"ip":"127.0.0.1","method":"GET","path":"/api","status":200,"timestamp":"2000-10-10T13:55:36Z"}"#;
+
+    let apache_parser = ApacheParser::new();
+    let nginx_parser = NginxParser::new();
+    let json_parser = JsonParser::new();
+
+    c.bench_function("apache_parse_single_line", |b| {
+        b.iter(|| apache_parser.parse(black_box(apache)))
+    });
+    c.bench_function("nginx_parse_single_line", |b| {
+        b.iter(|| nginx_parser.parse(black_box(nginx)))
+    });
+    c.bench_function("json_parse_single_line", |b| {
+        b.iter(|| json_parser.parse(black_box(json)))
+    });
+}
+criterion_group!(benches, bench_parse);
+criterion_main!(benches);
